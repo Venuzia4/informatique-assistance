56718490acb182e4c455d1d9f61ac631e3231bee
Venuzia Babongui Mabika <77842881+Venuzia4@users.noreply.github.com>
2022-02-24 22:38:26 +0100

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const message = "Thank you for submitting your request!"
-
-document
-.getElementById("contactForm").addEventListener("submit", function (event) {
-  event.preventDefault();
-  alert(message);
-});
-
